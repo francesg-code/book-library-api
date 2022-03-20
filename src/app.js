ed9dbad6f9fc98165c9eb52084eee6c8fa1b520b
francesg-code <75420565+francesg-code@users.noreply.github.com>
2022-03-20 11:27:36 +0000

    Create new entries with a POST request

--- src/app.js
+++ src/app.js
@@ -1,4 +1,11 @@
 const express = require("express");
+const readerControllers = require("./controllers/readers");
+
 const app = express();
+
+app.use(express.json());
+
+app.post("/readers", readerControllers.create);
+
 
 module.exports = app;
